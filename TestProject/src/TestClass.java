cf8a6845dc32484d9bc6f176d1ebfcd14d9c7cb4
ValabojuUdaykumar <ValabojuUdaykumar@DESKTOP-UT4SN5B>
2020-05-10 21:29:02 +0530

    Added addition logic

--- TestProject/src/TestClass.java
+++ TestProject/src/TestClass.java
@@ -1,9 +1,16 @@
 
 public class TestClass {
+	
+	int i,j,k;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		TestClass t=new TestClass();
+		t.i=10;
+		t.j=25;
+		t.k=t.i+t.j;
+		System.out.println("result is"+t.k);
 
 	}
 
